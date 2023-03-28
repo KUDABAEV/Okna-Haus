f39a17a0abe89d9ba3f3d3078dfbaac20531dc44
Arthur Kudabaev <arturo.kudabaev@mail.ru>
2023-03-28 11:30:41 +0500

    Project Okna-Haus

---
+++ js/main.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+
+    $('.header__burger').click(function(event){
+        $('.header__burger,.nav').toggleClass('active');
+        $('body').toggleClass('lock');
+    });
+
+});
